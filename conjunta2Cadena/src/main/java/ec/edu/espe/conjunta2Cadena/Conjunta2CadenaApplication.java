2494eda1ea62129825ba73edced8726b0c3a5cbd
pacadena5 <pacadena5@espe.edu.ec>
2023-01-18 10:18:21 -0500

    conjunta2 initial commit

---
+++ conjunta2Cadena/src/main/java/ec/edu/espe/conjunta2Cadena/Conjunta2CadenaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.conjunta2Cadena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Conjunta2CadenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Conjunta2CadenaApplication.class, args);
+	}
+
+}
---
+++ conjunta2Cadena/src/test/java/ec/edu/espe/conjunta2Cadena/Conjunta2CadenaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.conjunta2Cadena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Conjunta2CadenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
